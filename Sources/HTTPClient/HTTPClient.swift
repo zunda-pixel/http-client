dc175d119c5ca55f92f1a7bc35cc3ae6faf390ed
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-11 01:37:07 +0900

    add parameter

--- Sources/HTTPClient/HTTPClient.swift
+++ Sources/HTTPClient/HTTPClient.swift
@@ -3,5 +3,5 @@ import HTTPTypes
 public protocol HTTPClientProtocol: Sendable {
   associatedtype Body
   associatedtype Data
-  func execute(_ request: HTTPRequest, body: Body?) async throws -> (Data, HTTPResponse)
+  func execute(for request: HTTPRequest, body: Body?) async throws -> (Data, HTTPResponse)
 }
