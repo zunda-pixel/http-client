2d86354e5fc4ac7d7d643252d7727cdb8f471e75
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-13 03:44:08 +0900

    add platforms (#2)

--- Package.swift
+++ Package.swift
@@ -4,6 +4,14 @@ import PackageDescription
 
 let package = Package(
   name: "http-client",
+  platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .watchOS(.v6),
+    .tvOS(.v13),
+    .visionOS(.v1),
+    .macCatalyst(.v14),
+  ],
   products: [
     .library(
       name: "HTTPClient",
